d0a1abd0d0206147297973c2b6574be5428c54e4
KeyU_905 <ksg95v@gmail.com>
2021-06-17 23:46:29 +0900

    commit

--- src/main/java/edu/bit/ex/controller/RestBoardSpring4AfterController.java
+++ src/main/java/edu/bit/ex/controller/RestBoardSpring4AfterController.java
@@ -3,30 +3,32 @@ package edu.bit.ex.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import edu.bit.ex.service.BoardService;
 import edu.bit.ex.vo.BoardVO;
 import lombok.extern.log4j.Log4j;
 
-// spring v4.0 이전버전 (@Controller + @ResponseBody)
+// spring v4.0 이후 버전 
+// @RestController 라는 어노테이션을 추가해서 
+// 해당 Controller의 모든 메서드의 리턴타입을 기존과 다르게 처리한다는 것을 명시
 
 
 @Log4j
-@Controller
-public class RestBoardSpring4BeforeController {
+@RestController //(모든 메서드의 리턴타입을 기존과 다르게 처리한다는 것을 명시) 
+public class RestBoardSpring4AfterController {
 	
     @Autowired
     private BoardService boardService;
     
     
-    @ResponseBody//(메서드의 리턴타입을 기존과 다르게 처리한다는 것을 명시)
-	@GetMapping("/rest/before")
+    // 자바객체 list를 json 형태 바꾸어줌
+	@GetMapping("/rest/after")
 	public List<BoardVO> restBefore(Model model) {
-	    log.info("restBefore()...");
+	    log.info("restAfter()...");
 	    
 	    List<BoardVO> list = boardService.getList();
 	    
